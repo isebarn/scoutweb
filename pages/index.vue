023c61e9546a7bedfa77a96ecd637b558fb2a651
isebarn <isebarn182@gmail.com>
2020-07-27 14:31:44 +0000

    First

---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-data-table
+        :headers="headers"
+        :items="items"
+      >
+        <template v-slot:item.src="{ item }">
+          <img
+            :src="item.src"
+            style="width: 100px; height: 100px"
+            @click="openURL(item)"
+          >
+        </template>
+      </v-data-table>
+    </v-row>
+    <v-row>
+      <v-btn block color="success" @click="load">
+        text
+      </v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      items: [],
+      headers: [
+        {
+          text: '',
+          value: 'src'
+        },
+        {
+          text: 'price',
+          value: 'price'
+        },
+        {
+          text: 'provision',
+          value: 'provision'
+        },
+        {
+          text: 'size',
+          value: 'size'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    async load ({ $api }) {
+      const { data } = await this.$api.Data.get()
+      this.items = data
+    },
+
+    openURL (item) {
+      window.open(item.url)
+    }
+  }
+}
+</script>
